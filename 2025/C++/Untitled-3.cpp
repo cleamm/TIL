11563ee5c831aa3dd3f08cce87ce724ca8648e48
cleamm <ehdgus275@naver.com>
2025-07-11 17:05:51 +0900

    c++ update

--- 2025/C++/Untitled-3.cpp
+++ 2025/C++/Untitled-3.cpp
@@ -24,15 +24,58 @@ int main(){
 
 
 
-    // 분기
-    int num;
-    cout << "숫자를 입력하세요:";
-    cin >> num; 
-    if (num > 10){
-        cout << "10보다 큽니다." << endl;
+    // 분기1 if문
+    // int num;
+    // cout << "숫자를 입력하세요:";
+    // cin >> num; 
+    // if (num > 10){
+    //     cout << "10보다 큽니다." << endl;
+    // }else if ( num == 10){
+    //     cout << "10입니다." << endl;
+    // }else{
+    //     cout << "10보다 작습니다." << endl;
+    // }
+
+    // 분기2 : 삼항 연산자
+    // cout << (num % 2 == 0 ? "짝수임." : "홀수임.") << endl;
     
-    }else{
-        cout << "10보다 작습니다." << endl;
+
+    // 분기3 : switch - case
+    // int num = 10;
+    // switch(num){ // 해당 조건과 같은지만 case에서 비교하게 됨
+    //     case 0:
+    //         cout << "0입니다." << endl;
+    //         break; // break가 없으면 모든 case를 실행할 수도 있으니 주의
+    //     case 1:
+    //         cout << "1입니다." << endl;
+    //         break;
+    //     case 2:
+    //         cout << "2입니다." << endl;
+    //         break;
+    //     default:
+    //         cout << "예상 외의 값임" << endl;
+    //         // 마지막 break는 생략이 가능함
+    // }
+
+
+    // 반복문1 for문
+    // for (int i = 0; i < 10; i++){
+    //     cout << i;
+    // }cout << endl;
+
+    // int arr[] = {1,2,3,3,4,3,3,3,4,5,5,6}; // 배열 길이 구하기
+    // printf("%d", sizeof(arr)/sizeof(int));
+    // for (int i = 0; i < sizeof(arr)/sizeof(int); i++){
+    //     cout << arr[i];
+    // }
+    // cout << endl;
+
+
+    char my_str[] = "im steel hungry...";
+    for (int i = 0; i < sizeof(my_str)/sizeof(char); i++){
+        printf("%c", my_str[i]);
+        printf("%d", i);
+        printf("\n");
     }
     return 0;
 }
